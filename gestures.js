cefb9b15c84b8ed60a68eb876caf5f7d4374be81
vainillaylatte <dbfr312@gmail.com>
2017-01-12 09:28:04 -0600

    Add files via upload

---
+++ gestures.js
@@ -0,0 +1,27 @@
+var app={
+	inicio: function(){
+		
+		var botonClaro = document.querySelector('#claro');
+		var botonOscuro = document.querySelector('#oscuro');
+		
+		botonClaro.addEventListener('click',this.ponloClaro,false);
+		botonOscuro.addEventListener('click',this.ponloOscuro,false);
+	},
+	
+	ponloClaro: function(){
+		document.body.className = 'claro';
+	},
+	
+	ponloOscuro: function(){
+		document.body.className = 'oscuro';
+	},
+	
+};
+
+if ('addEventListener' in document) {
+	document.addEventListener('DOMContentLoaded', function() {
+		FastClick.attach(document.body);
+		app.inicio();
+	}, false);
+}
+	
